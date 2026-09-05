7a89770bc7df9b4785cc207c6bc995e2e0721332
bansalshorya <bansalshorya625@gmail.com>
2026-09-05 18:27:19 +0530

    21 patterns

--- src/Pattern/src/CategoryTwo/ConcentricHollowSquares.java
+++ src/Pattern/src/CategoryTwo/ConcentricHollowSquares.java
@@ -1,11 +1,26 @@
 package CategoryTwo;
+import java.util.Scanner;
 
 public class ConcentricHollowSquares {
-    public static void main() {
-        System.out.println("Concentric Hollow Squares");
-        int n=7;
-        for (int i = 1; i <=n ; i++) {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the square (odd number, e.g., 7): ");
+        int n = sc.nextInt();
 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Compute minimum distance to the outer perimeter
+                int d = Math.min(Math.min(i, j), Math.min(n - 1 - i, n - 1 - j));
+
+                // If the distance is even, print an asterisk; otherwise, print a space
+                if (d % 2 == 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
         }
+        sc.close();
     }
 }
